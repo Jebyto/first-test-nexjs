c063a5e233eaa91284ceff7fd5af3c9aaa1b985b
José Carlos <jose-carlos64@live.com>
2024-06-09 00:49:25 -0300

    Rotas dos produtos

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,22 +1,25 @@
 // pages/cart.tsx
 
+import Header from "@/components/Header";
 import { NextPage } from "next";
 import Head from "next/head";
 
 const Cart: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Carrinho</title>
-        <meta name="description" content="Meu carrinho de compras" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <>
+            <Head>
+                <title>Carrinho</title>
+                <meta name="description" content="Meu carrinho de compras" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <h1>
-        Carrinho
-      </h1>
-    </>
-  )
+            <Header />
+
+            <h1>
+                Carrinho
+            </h1>
+        </>
+    )
 }
 
 export default Cart
